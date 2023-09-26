e4bca7b3e35fa592546642cf22fcb422947556ec
ripassorker2 <ripassorkar08@gmail.com>
2023-09-26 14:11:49 +0600

    project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className="text-primary text-6xl">I am from navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
