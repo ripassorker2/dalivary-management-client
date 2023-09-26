811a2d3fbb640f358f823eea5ebf71d707763575
ripassorker2 <ripassorkar08@gmail.com>
2023-09-27 01:15:52 +0600

    nothing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
             <h1 className="text-primary text-6xl text-center ">
                 I am from navbar. I am coming from begom para...
             </h1>
+            <h1>You sosur bari is bodaaa...</h1>
         </div>
     );
 };
