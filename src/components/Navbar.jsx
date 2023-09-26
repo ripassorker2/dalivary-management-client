a526e491aa9c02f84b8e15193350d565374b2942
ripassorker2 <ripassorkar08@gmail.com>
2023-09-27 00:39:24 +0600

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Navbar = () => {
     return (
         <div>
-            <h1 className="text-primary text-6xl">I am from navbar</h1>
+            <h1 className="text-primary text-6xl text-center ">
+                I am from navbar. I am coming{" "}
+            </h1>
         </div>
     );
 };
